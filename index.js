257ec060470637338af98dbc309c4d136cdae722
Douglas Drouillard <drouillard@gmail.com>
2015-10-07 11:43:42 -0400

    Update for deploy to Heroku

---
+++ index.js
@@ -0,0 +1,10 @@
+// project/index.js
+var jsonServer = require('json-server');
+
+var server = jsonServer.create();
+var router = jsonServer.router('./db.json');
+
+server.use(jsonServer.defaults());
+server.use(router);
+
+server.listen(3000);
